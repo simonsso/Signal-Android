mollyim/mollyim-android

Processed review comments

app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
@@ -226,9 +226,7 @@ public class ConversationListFragment extends MainFragment implements LoaderMana
     initializeTypingObserver();
     initializeSearchListener();
 
-    if (!TextSecurePreferences.isPinV2ReminderDisabled(requireContext())) {
-      RegistrationLockDialog.showReminderIfNecessary(this);
-    }
+    RegistrationLockDialog.showReminderIfNecessary(this);
 
     TooltipCompat.setTooltipText(searchAction, getText(R.string.SearchToolbar_search_for_conversations_contacts_and_messages));
   }

simonsso/Signal-Android

app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
@@ -236,9 +236,8 @@ public class ConversationListFragment extends MainFragment implements LoaderMana
     initializeTypingObserver();
     initializeSearchListener();
 
-    RatingManager.showRatingDialogIfNecessary(requireContext());
     if (!TextSecurePreferences.isPinV2ReminderDisabled(requireContext())) {
-        RegistrationLockV1Dialog.showReminderIfNecessary(this);
+      RegistrationLockV1Dialog.showReminderIfNecessary(this);
     }
 
     TooltipCompat.setTooltipText(searchAction, getText(R.string.SearchToolbar_search_for_conversations_contacts_and_messages));