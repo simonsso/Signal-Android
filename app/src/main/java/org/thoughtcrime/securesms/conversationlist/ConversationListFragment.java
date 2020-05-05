mollyim/mollyim-android

Added an option to not show v2 PIN reminders.

app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
@@ -226,7 +226,9 @@ public class ConversationListFragment extends MainFragment implements LoaderMana
     initializeTypingObserver();
     initializeSearchListener();
 
-    RegistrationLockDialog.showReminderIfNecessary(this);
+    if (!TextSecurePreferences.isPinV2ReminderDisabled(requireContext())) {
+      RegistrationLockDialog.showReminderIfNecessary(this);
+    }
 
     TooltipCompat.setTooltipText(searchAction, getText(R.string.SearchToolbar_search_for_conversations_contacts_and_messages));
   }

simonsso/Signal-Android

app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
@@ -237,8 +237,9 @@ public class ConversationListFragment extends MainFragment implements LoaderMana
     initializeSearchListener();
 
     RatingManager.showRatingDialogIfNecessary(requireContext());
-
-    RegistrationLockV1Dialog.showReminderIfNecessary(this);
+    if (!TextSecurePreferences.isPinV2ReminderDisabled(requireContext())) {
+        RegistrationLockV1Dialog.showReminderIfNecessary(this);
+    }
 
     TooltipCompat.setTooltipText(searchAction, getText(R.string.SearchToolbar_search_for_conversations_contacts_and_messages));
   }